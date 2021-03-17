58a6bf04466c4ee51cfbb1ec2e99838a39dd460f
Ramar Faleiro <rtawera@gmail.com>
2021-03-17 15:44:23 -0300

    baseline

--- js/setup.js
+++ js/setup.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     $(".owl-carousel").owlCarousel({
-        loop: true,
+        loop: false,
         margin: 10,
         nav: false,
         responsive: {
@@ -67,32 +67,55 @@ document.addEventListener('scroll', function (e) {
 //
 
 function addItem(listItem, id) {
-    console.log('Apensou')
+    //div.item
     var div = document.createElement('div');
     $(div).addClass('item');
+
+    //div.overlay
+    var overlay = document.createElement('div');
+    $(overlay).addClass('thumb-overlay');
+
+    //title
+    var title = document.createElement('h2');
+    title.innerHTML = listItem.title;
+    $(overlay).append(title);
+
+    //video
     var novoVideo = document.createElement('video');
     $(novoVideo).addClass('thumb-video');
     $(novoVideo).attr('preload', 'none');
     $(novoVideo).attr('height', '600px');
     $(novoVideo).attr('width', '100%');
-    $(novoVideo).attr('poster', listItem.poster);
+    //source
+    $(source).attr('preload', 'none');
     var source = document.createElement('source');
     $(source).attr('src', listItem.src);
     $(source).attr('type', 'video/mp4');
-    $(novoVideo).append(source);
-    $(div).prepend(novoVideo);
 
+    //apensos    
+    $(novoVideo).append(source);
+    $(div).prepend(overlay);
+    $(div).append(novoVideo);
     $('#' + id).append($(div));
 };
 
 //Series
-var series = [{
-    poster: "/img/demolidor.jpg",
-    src: "/src/demolidor.mp4"
-}, {
-    poster: "/img/pokemon.jpg",
-    src: "/src/pokemon.mp4"
-}];
+var series = [
+    {
+        poster: "/img/pokemon.jpg",
+        title: 'Will Smith descarrega 6000 tijolos',
+        src: "/src/smith.mp4"
+    },
+    {
+        poster: "/img/demolidor.jpg",
+        src: "/src/demolidor.mp4"
+    }, {
+        poster: "/img/pokemon.jpg",
+        src: "/src/pokemon.mp4"
+    }, {
+        poster: "/img/cafe.png",
+        src: "/src/cafe.mp4"
+    }];
 
 var filmes = [{
     poster: "/img/starwars.jpg",
@@ -115,33 +138,37 @@ filmes.forEach(listItem => {
     addItem(listItem, 'carrosel-filme');
 });
 
+var itens = document.getElementsByClassName('item');
+$(itens).map(function (value, item) {
+    //console.log(item);
+    item.addEventListener('click', function () {
+        let index = $(item).find('video');
 
-//Thumbs-video
-function reset(paramVideo) {
-    paramVideo.pause();
-    paramVideo.currentTime = 0;
-    paramVideo.load();
-    paramVideo.pause();
-}
+        if ($(index).get(0).currentTime > 0) {
+            $(item).find('div').css('display', 'block');
 
-function playThumbVideo(thumbVideo){
-    thumbVideo.removeEventListener("click", playThumbVideo);
-
-    thumbVideo.addEventListener('click', function () {
-        reset(thumbVideo);
-    });
-    thumbVideo.addEventListener('mouseout', function () {
-        reset(thumbVideo);
-    });
-    setTimeout(function () {
-        thumbVideo.play();
-    }, 1000)
-}
+            $(index).get(0).pause();
+            $(index).currentTime = 0;
+            $(index).get(0).load();
+            $(index).get(0).pause();
+        } else {
+            $(item).find('div').css('display', 'none');
+            //reset(index);            
+            /*$(index).on('mouseout', function () {
+                $(index).get(0).pause();
+                $(index).get(0).currentTime = 0;
+                $(index).get(0).load();
+                $(index).get(0).pause();
+            });*/
+            $(index).get(0).play();
+        }
+    })
+});
 
 var videos = document.getElementsByClassName('thumb-video');
-
 $(videos).map(function (value, index) {
-    //console.log(value);    
-    console.log(index);
-    listenerPlay = index.addEventListener('click', playThumbVideo);
+    index.pause();
+    index.currentTime = 0;
+    index.load();
+    index.pause();
 })
